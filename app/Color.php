9e95f483ba47e9dfb180d932e15172b58f07c622
el3zahaby <el3zahaby@gmail.com>
2020-04-26 18:11:43 +0300

    add use SoftDeletes to colorModel

--- app/Color.php
+++ app/Color.php
@@ -3,13 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use TCG\Voyager\Traits\Translatable;
 
 
 class Color extends Model
 {
-    // use Translatable;
-    protected $translatable = ['name'];
+    use SoftDeletes;
     protected $guarded = [];
 
     public static function getNameById($id)
